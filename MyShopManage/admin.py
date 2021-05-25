e6087c6d1e3ec6d01c804003016d40e6ed2661c5
Aung Ko Zin <73013105+Sniper445-cell@users.noreply.github.com>
2021-05-25 11:31:17 +0630

    Add files via upload

---
+++ MyShopManage/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(ShopList)
+admin.site.register(ShopManager)
+admin.site.register(Shopemployee)
+
+
+# Register your models here.
